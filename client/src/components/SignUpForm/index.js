a9f6f34ae16dce972e7309cdf5329da31b5c8393
Thalia Rodriguez <thaliarpuron@gmail.com>
2020-12-04 00:46:57 -0500

    another update

--- client/src/components/SignUpForm/index.js
+++ client/src/components/SignUpForm/index.js
@@ -1,33 +1,34 @@
 import React from 'react';
-import image from './logo.png';
+import logo from './logo.png';
 import "./style.css";
 
 const SignUpForm = (props) => {
-    
-    
+
     return <div>
 
-        <nav className="navbar navbar-default">
+        <nav className="navbar navbar-light bg-light">
             <div className="container-fluid">
-                <div className="top-bar text-center">
-                    <img className="logo" src={image} />
+                <div></div>
+                <div className="top-bar text-center u-text-centered">
+                    <img className="logo" src={logo} />
                     <p className="navBarText">Places to Go</p>
                 </div>
+                <div></div>
             </div>
         </nav>
-        <hr />
 
 
         <div className="container">
             <div className="row">
                 <div className="col-md-6 col-md-offset-3 card card-body">
                     <h2>Sign Up</h2>
+                    <hr />
                     <form className="signup">
-                    <label for="exampleInputEmail">Email Address:</label>
-                    <input type="email" className="form-control" id="email-input" placeholder="Email" />
-                    <label for="exampleInputPassword">Password:</label>
-                    <input type="password" className="form-control" id="password-input" placeholder="Password"/>
-                    <button type="submit" className="btn btn-success signupBTN">Sign Up</button>
+                        <label for="exampleInputEmail">Email Address:</label>
+                        <input type="email" className="form-control" id="email-input" placeholder="Email" />
+                        <label for="exampleInputPassword">Password:</label>
+                        <input type="password" className="form-control" id="password-input" placeholder="Password" />
+                        <button type="submit" className="btn btn-success signupBTN">Sign Up</button>
                     </form>
                 </div>
             </div>
